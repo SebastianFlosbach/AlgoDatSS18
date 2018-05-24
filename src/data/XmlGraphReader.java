cc7ae2d47c1162adec846a8876ebdec404859b17
SebastianFlosbach <sebflos@online.de>
2018-05-24 13:58:36 +0200

    Added data package Added XmlGraphReader

---
+++ src/data/XmlGraphReader.java
@@ -0,0 +1,17 @@
+package data;
+
+import entities.graph.Graph;
+
+public class XmlGraphReader {
+	
+	private String m_Path;
+	
+	public XmlGraphReader(String _path) {
+		m_Path = _path;
+	}
+	
+	public Graph ReadGraph() {
+		return null;
+	}
+	
+}
