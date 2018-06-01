55c6c094359698bf8158451c304bd04dd114a53a
KrugDavid <david.krug@gmx.net>
2018-06-01 17:34:44 +0200

    Prim works with an adjacency matrix like in the script (without your Graph class)

--- src/business/algorithm/Prim.java
+++ src/business/algorithm/Prim.java
@@ -1,5 +1,69 @@
 package business.algorithm;
 
 public class Prim {
+    
+    private final int infinite = Integer.MAX_VALUE - 1;
+    private int[] priority;
+    private int[] parent;
+    
+    //Prim's algorithm with an adjacency matrix as input
+    public void MatrixPriorityFirst(int input[][])
+    {
+
+        int n = input.length+1;
+        parent = new int[n];
+        priority = new int[n];
+
+        Init();
+
+        int min=0;
+        int k;
+        int prio;
+        do {
+            k=min;
+            priority[k]=-priority[k];
+            min=n-1;
+            if(priority[k]==infinite)
+            {
+                priority[k]=0;
+            }
+            for(int t = 0;t<n-1;t++)
+            {
+                prio=-input[k][t];
+
+                if(priority[t]<0)
+                {
+                    if(input[k][t]>0 && (priority[t]<prio))
+                    {
+                        priority[t]=prio;
+                        //Graph starts with 0
+                        parent[t]=k;
+                    }
+                    if(priority[t]>priority[min])
+                    {
+                        min=t;
+                    }
+                }
+            }
+        }while(min!=input.length);
+
+        System.out.println("Edges       Weights");
+
+        for (int i=1;i<n-1;i++)
+        {
+            System.out.println(i+" - "+parent[i]+"           " + priority[i]);
+        }
+    }
+
+
+    //Initialize both arrays
+    private void Init() {
+        for (int i=0;i<priority.length;i++)
+        {
+            priority[i]=-infinite;
+            parent[i]=Integer.MAX_VALUE;
+        }
+        priority[priority.length-1]=-(infinite+1);
+    }
 
 }
--- src/presentation/Main.java
+++ src/presentation/Main.java
@@ -1,6 +1,7 @@
 package presentation;
 
 import business.algorithm.Dijkstra;
+import business.algorithm.Prim;
 import entities.graph.Graph;
 import entities.graph.Path;
 import data.XmlGraphReader;
@@ -11,19 +12,25 @@ public class Main {
 		
 		System.out.println("Program start");
 		
-		if(args.length > 0 && args[0] != null) {
-			XmlGraphReader reader = new XmlGraphReader(args[0]);
-			
-			Graph graph = reader.ReadGraph();
-			
-			Path path = Dijkstra.FindShortestPath(graph, 5, 2);
-			
-			System.out.println(path);
-		}
-		else {
-			System.out.println("No valid argument");
-		}
+//		if(args.length > 0 && args[0] != null) {
+//			XmlGraphReader reader = new XmlGraphReader(args[0]);
+//
+//			Graph graph = reader.ReadGraph();
+//
+//			Path path = Dijkstra.FindShortestPath(graph, 5, 2);
+//
+//			System.out.println(path);
+//		}
+//		else {
+//			System.out.println("No valid argument");
+//		}
 
+
+		int input[][] = new int[][]{{0,2,0,6,0},{2,0,3,8,5},{0,3,0,0,7},{6,8,0,0,9},{0,5,7,9,0}};
+
+		Prim test = new Prim();
+
+		test.MatrixPriorityFirst(input);
 	}
 
 }
