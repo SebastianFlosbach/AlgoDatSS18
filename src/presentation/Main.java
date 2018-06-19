8f1d573fb375bfdba2bfa6fdbba3d1d506103a56
SebastianFlosbach <sebflos@online.de>
2018-06-19 19:37:43 +0200

    dummy commit

--- src/presentation/Main.java
+++ src/presentation/Main.java
@@ -1,11 +1,9 @@
 package presentation;
 
-import business.algorithm.MatrixPriorityFirst;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
+/*		
 		if(args != null && args.length > 0) {
 			GraphTester tester = new GraphTester(args[0]);
 			
@@ -14,12 +12,9 @@ public class Main {
 		else {
 			System.out.println("Missing argument: Path to graphs!");
 		}
-
-		/*int[][] matrix = new int[][]{{0,2,0,6,0},{2,0,0,8,0},{0,0,0,0,7},{6,8,0,0,0},{0,0,7,0,0}};
-
-		MatrixPriorityFirst mpf = new MatrixPriorityFirst();
-		mpf.doPrim(matrix);
-		mpf.doDijkstra(matrix);*/
+*/
+		
+		
 		
 		
 		
